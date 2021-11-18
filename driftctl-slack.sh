7b3c1737db695077425b08a1d3a2b0464d7f481b
cruftyoldsysadmin <cruftyoldsysadmin@users.noreply.github.com>
2021-11-18 19:06:23 +0300

    Changes some formatting outputs

--- driftctl-slack.sh
+++ driftctl-slack.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 echo "Beginning scan"
-/usr/local/bin/driftctl scan --from tfstate+s3://"${BUCKET_NAME}"/**/*.tfstate -o console://,json://"${DRIFTCTL_JSON}" || exit 1
+/usr/local/bin/driftctl scan --from tfstate+s3://"${BUCKET_NAME}"/**/*.tfstate -o json://"${DRIFTCTL_JSON}" || exit 1
 echo "Sending scan to slack"
 /usr/local/bin/driftctl-slack 
