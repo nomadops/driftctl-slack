dfb70c440a3171525daafda615615783089f561c
cruftyoldsysadmin <cruftyoldsysadmin@users.noreply.github.com>
2021-11-18 18:19:06 +0300

    Changes bash location in entrypoint script

--- driftctl-slack.sh
+++ driftctl-slack.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+echo "foo"
 /usr/local/bin/driftctl scan --from tfstate+s3://"${BUCKET_NAME}"/**/*.tfstate -o console://,json://"${DRIFTCTL_JSON}" || exit 1
+echo "bob"
 /usr/local/bin/driftctl-slack 
