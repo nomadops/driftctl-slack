c9430f0197ff04ac2fcd2b7a661698f7e968e758
cruftyoldsysadmin <cruftyoldsysadmin@users.noreply.github.com>
2021-12-04 00:31:00 +0300

    Adds zerolog json logging

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/logshell/driftctl-slack
 go 1.16
 
 require (
-	github.com/fatih/structs v1.1.0 // indirect
-	github.com/slack-go/slack v0.10.0
+	github.com/rs/zerolog v1.26.0 // indirect
+	github.com/slack-go/slack v0.10.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,48 +2,71 @@ package main
 
 import (
 	"io/ioutil"
-	"log"
 	"os"
 
 	"github.com/logshell/driftctl-slack/driftctl"
 	driftslack "github.com/logshell/driftctl-slack/slack"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 )
 
 func main() {
+	// Apply zerolog global level, this will stop zerolog from logging any debug messages
+	debug := false
+	// Apply log level in the beginning of the application
+	zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	if debug {
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	}
+
 	// get driftctl scan output filename from the environment
 	driftctlJSON, err := os.LookupEnv("DRIFTCTL_JSON")
 	if !err {
-		log.Fatal("Environment variable DRIFTCTL_JSON does not exist")
+		log.Fatal().Msg("Environment variable DRIFTCTL_JSON does not exist")
 	}
 
 	// get slack channel from the environment
 	slackChannel, err := os.LookupEnv("SLACK_CHANNEL")
 	if !err {
-		log.Fatal("Environment variable SLACK_CHANNEL does not exist")
+		log.Fatal().Msg("Environment variable SLACK_CHANNEL does not exist")
 	}
 
 	// Get slack token from the environment
 	slackToken, err := os.LookupEnv("SLACK_TOKEN")
 	if !err {
-		log.Fatal("Environment variable SLACK_TOKEN does not exist")
+		log.Fatal().Msg("Environment variable SLACK_TOKEN does not exist")
 	}
 
 	// Read driftctl scan output file
 	content, err1 := ioutil.ReadFile(driftctlJSON)
 	if err1 != nil {
-		log.Fatal("Error when opening file: ", err)
+		log.Fatal().
+			Bool("Error while opening file:", err).
+			Msg("")
 	}
 
 	// Get Driftctl scan summary
 	slackMessage, err1 := driftctl.ScanSummary(content)
 	if err1 != nil {
-		log.Fatal("Error when opening file: ", err)
+		log.Fatal().
+			Bool("Error when opening file: ", err).
+			Msg("")
 	}
+	log.Info().
+		Str("service", "driftctl-slack").
+		Int("total_resources", slackMessage["total_resources"]).
+		Int("total_changed", slackMessage["total_changed"]).
+		Int("total_unmanaged", slackMessage["total_unmanaged"]).
+		Int("total_missing", slackMessage["total_missing"]).
+		Int("total_managed", slackMessage["total_managed"]).
+		Msg("Driftctl scan summary")
 
 	// Send driftctl scan summary output to slack
 	driftslack.SendSummary(slackToken, slackChannel, slackMessage)
 	if !err {
-		log.Fatal("Error when running driftctl.ScanSummary", err)
+		log.Fatal().
+			Bool("Error when running driftctl.ScanSummary", err).
+			Msg("")
 	}
 }
 
