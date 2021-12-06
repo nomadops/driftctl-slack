304fd94474ff11ad3a2a9b2add843616bab3dbec
cruftyoldsysadmin <cruftyoldsysadmin@users.noreply.github.com>
2021-12-06 18:57:47 +0300

    Copies driftctl scan output to s3 bucket

--- driftctl-slack.sh
+++ driftctl-slack.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-echo "Echoing slack token"
-echo "${SLCK_TOKEN}"
-env
 /usr/local/bin/driftctl scan --from tfstate+s3://"${BUCKET_NAME}"/**/*.tfstate -o json://"${DRIFTCTL_JSON}"
 echo "Sending scan to slack"
 /usr/local/bin/driftctl-slack 
+echo "Copying scan to s3"
+aws cp "${DRIFTCTL_JSON}" s3://"${SCAN_BUCKET_NAME}"/driftctl-scan.json
