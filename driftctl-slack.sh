4daf9e04112c674383f12b9c933ccf3a1b773e80
cruftyoldsysadmin <cruftyoldsysadmin@users.noreply.github.com>
2021-12-06 19:20:35 +0300

    Fixes typo in driftctl-slack.sh

--- driftctl-slack.sh
+++ driftctl-slack.sh
@@ -4,4 +4,4 @@
 echo "Sending scan to slack"
 /usr/local/bin/driftctl-slack 
 echo "Copying scan to s3"
-aws cp "${DRIFTCTL_JSON}" s3://"${SCAN_BUCKET_NAME}"/driftctl-scan.json
+aws s3 cp "${DRIFTCTL_JSON}" s3://"${SCAN_BUCKET_NAME}"/driftctl-scan.json
