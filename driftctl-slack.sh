26e93ab234e91fd284fad163c63d2d11ce827ebe
cruftyoldsysadmin <cruftyoldsysadmin@users.noreply.github.com>
2021-12-06 20:50:24 +0300

    Adds environment variable validation to driftctl-slack.sh

--- driftctl-slack.sh
+++ driftctl-slack.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+if [[ ${BUCKET_NAME:-"unset"} == "unset" ]]; then
+  echo "BUCKET_NAME is not set"
+  exit 1
+fi
+
+if [[ ${SCAN_BUCKET_NAME:-"unset"} == "unset" ]]; then
+  echo "SCAN_BUCKET_NAME is not set"
+  exit 1
+fi
+
+if [[ ${DRIFTCTL_JSON:-"unset"} == "unset" ]]; then
+  echo "BUCKET_NAME is not set"
+  exit 1
+fi
+
 /usr/local/bin/driftctl scan --from tfstate+s3://"${BUCKET_NAME}"/**/*.tfstate -o json://"${DRIFTCTL_JSON}"
 echo "Sending scan to slack"
 /usr/local/bin/driftctl-slack 
