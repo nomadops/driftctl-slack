e9eaf0dbc301dc65ab9c183bf386edceb501ee98
cruftyoldsysadmin <cruftyoldsysadmin@users.noreply.github.com>
2021-12-08 23:28:38 +0300

    Adds json logging to all lines of the driftctl file

--- driftctl-slack.sh
+++ driftctl-slack.sh
@@ -1,22 +1,42 @@
 #!/bin/bash
 
+# PID of current shell script
+PID=$$
+
+# Name of current shell script
+CMD=$(basename "${0}")
+
+# Formats basic syslog messages as JSON 
+log() {
+  jq --arg message "${1}" --arg hostname "${HOSTNAME}" --arg service "${CMD}[${PID}]" -nc '{"@timestamp": now|strftime("%Y-%m-%dT%H:%M:%S%z"),"hostname": $hostname, "service": $service, "message": $message}'
+}
+
+# Check for BUCKET_NAME environment variable
 if [[ ${BUCKET_NAME:-"unset"} == "unset" ]]; then
-  echo "BUCKET_NAME is not set"
+  log "BUCKET_NAME is not set"
   exit 1
 fi
 
+# Check for SCAN_BUCKET_NAME environment variable
 if [[ ${SCAN_BUCKET_NAME:-"unset"} == "unset" ]]; then
-  echo "SCAN_BUCKET_NAME is not set"
+  log "SCAN_BUCKET_NAME is not set"
   exit 1
 fi
 
+# Check for DRIFTCTL_JSON environment variable
 if [[ ${DRIFTCTL_JSON:-"unset"} == "unset" ]]; then
-  echo "BUCKET_NAME is not set"
+  log "BUCKET_NAME is not set"
   exit 1
 fi
 
+# Run a driftctl scan
+log "Running driftctl scan"
 /usr/local/bin/driftctl scan --quiet --from tfstate+s3://"${BUCKET_NAME}"/**/*.tfstate -o json://"${DRIFTCTL_JSON}"
-echo "Sending scan to slack"
+
+# Publish driftsctl scan summary to slack
+log "Sending driftctl scan summary to slack"
 /usr/local/bin/driftctl-slack 
-echo "Copying scan to s3"
-aws s3 --output json cp "${DRIFTCTL_JSON}" s3://"${SCAN_BUCKET_NAME}"/"${DRIFTCTL_JSON}"
+
+# Copy driftctl scan summary to S3
+log "Copying ${DRIFTCTL_JSON} to s3 ${SCAN_BUCKET_NAME}"
+aws s3 cp "${DRIFTCTL_JSON}" s3://"${SCAN_BUCKET_NAME}"/"${DRIFTCTL_JSON}"
